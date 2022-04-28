25902bf694ead2973f1b3b18ca06a84385f1c409
David Calavera <dcalaver@amazon.com>
2022-04-28 15:40:40 +0000

    Update domains
    Signed-off-by: David Calavera <dcalaver@amazon.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['assets.vercel.com'],
+    domains: ['assets.vercel.com', 'main.d3pwjf9sx3jb3e.amplifyapp.com'],
     formats: ['image/avif', 'image/webp'],
   },
 }
